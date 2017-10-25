3f50bad4852eb87c696ec08aa5e4a41ed2f7d5c3
angelo <aq211@exeter.ac.uk>
2017-10-25 15:03:52 +0100

    wed am

--- src/js/app3.js
+++ src/js/app3.js
@@ -1,43 +1,90 @@
 // globals - gulp
-var candidateExample
 var categories = []
-main()
+var candidateExample
+
+function setup() {
+  var dropZone = select('#dropZone')
+                 .dragOver(highlight)
+                 .dragLeave(unhighlight)
+                 .drop(gotFile, unhighlight);
+  
+  main()
+}
+
+function gotFile(file) {
+  var maxBytes = 10000
+  // createP(file.name)
+  // createP(file.type)
+  createP(file.size)
+  // createP(file.data)
+
+  // check for text file
+  if (file.type != 'text') {
+    // give graphic indication of problem
+    alert('can only process textual files such as csv')
+    return 
+  }
+
+  // filesize check?
+  if (file.size > maxBytes) {
+    alert('file too big')
+    return 
+  }
+
+  // todo rollup dropZone div
+
+  const fileData = file.data
+  const rows = fileData.split("\n");
+  // console.log(splitString)
+  console.table(rows)
+
+
+
+
+  // print out first 5 lines of data
+}
+
 
 function main() {
-  getCSV("/data/miniergug2009.csv")  
+
+
+  // getCSV("/data/miniergug2009.csv")  
+}
+
+// todo highlight toggle
+function highlight() {
+  this.style('background-color','#ccc')
+}
+function unhighlight() {
+  this.style('background-color','#555')
+
 }
 
 
 function getCSV(csvFile) {
-  var rawData
   var candidates = []
+
   d3.csv(csvFile, (dataRow) => {
     candidates.push(dataRow)
-  }, () => {
-    // qq we now have array of data objects!
+  }, (d) => {
+    // qq push d onto array directly?
     console.log('data loaded')
     console.table(candidates)
-    // console.log(candidates[0])
-    // console.log(candidates[0]['name'])
     
     // check we have some data, at least two (or three if one is names)
     if (candidates.length < 2) {
       console.log("can't compare only one candidate - bad data")
     } else {
       // all good, now process the data we've got
-      processCsv(candidates[0])
+      candidateExample = candidates[0]
+      processCsv(candidateExample)
     }
   })
 }
 
 
 function processCsv(candidate) {
-  candidateExample = candidate
   showExample(candidate)
-  // var measurables = selectMeasurables(candidate)
-  // if (measurables) {
-  //   var minimised = selectMinimised(candidate)
-  // }
   showMeasurables(candidate)
 }
 
@@ -45,18 +92,17 @@ function processCsv(candidate) {
 // show example data entry
 function showExample(candidate) {
   // first just show an example
-  // need to manipulate innerHTML of #exampleTable
   var html = ''
-  var table = document.getElementById('exampleTable')
-  // html += "<table>"
-  // for each prop of candidate, need row of names, then row of values
+  var property, propertyName
+  var table = getEl('exampleTable')
+
   html += "<tr><th></th>"
-  for (var propertyName in candidate) {
+  for (propertyName in candidate) {
     html += "<th>" + propertyName + "</th>"
     categories.push(propertyName)
   }
   html += "</tr><tr><td></td>"
-  for (var property in candidate) {
+  for (property in candidate) {
     html += "<td>" + candidate[property] + "</td>"
   }
   html += "</tr>"
@@ -67,41 +113,36 @@ function showExample(candidate) {
 
 // show and get measurable categories
 function showMeasurables(candidate) {
-  // console.log(candidate)
-  var html = ''
-  var property
   var i = 0
+  var html = ''
+  var table = getEl('exampleTable')
+  var n_props = Object.keys(candidate).length
+
   html += "<tr id='rankRow'>"
   html += "<td>rankable?</td>"
-  for (property in candidate) {
+  for (i; i<n_props; i++) {
     html += "<td><input type='checkbox' id='meas" + i +"'></td>"
-    i++
   }
   html += "</tr>"
-  var n_props = Object.keys(candidate).length
   html += "<tr id='rankHelp'>"
   html += "<td colspan='" + (n_props+1) + "'>" 
   html += "select at least two criteria to be used for ranking... "
+  html += "<br>eg don't include names etc... "
   html += "<button onclick='processMeasurables()'> done </button>"
   html += "</td></tr>"
-  var table = document.getElementById('exampleTable')
   table.insertAdjacentHTML('beforeend', html)
 }
 
 
 function processMeasurables() {
-  // console.log(candidateExample)
-  // console.log(categories)
   var measurables = []
   var i = 0
   var cat, catID
   var n_props = Object.keys(candidateExample).length
 
   for (i; i<n_props; i++) {
-    catID = 'meas' + i
-    // console.log(i, catID)
-    if (document.getElementById(catID).checked) {
-      var name  // category name?? 
+    // catID = 'meas' + i
+    if (document.getElementById('meas' + i).checked) {
       measurables.push(categories[i])
     }
   }
@@ -113,14 +154,18 @@ function processMeasurables() {
     console.log('got measurables:', measurables)
     // return measurables
     // change css colour of #rankRow
-    el = document.getElementById('rankRow')
-    el.style.background = 'blue'
+    el = getEl('rankRow')
+    // el.style.background = $gray
+    el.className  = 'rowLit'
     // remove #rankHelp row completely
-    el = document.getElementById('rankHelp')
+    el = getEl('rankHelp')
     
     // many ways to skin cat!
     // el.style.display = 'none'
     el.className = 'hidden'
+
+    el = getEl('rankRow')
+    el.className = 'rowDone'
 
     // todo - make boxes read-only?
 
@@ -153,7 +198,7 @@ function selectMinimised(candidate) {
   html += "select criteria where low values are better... "
   html += "<button onclick='processMinimised()'> done </button>"
   html += "</td></tr>"
-  var table = document.getElementById('exampleTable')
+  var table = getEl('exampleTable')
   table.insertAdjacentHTML('beforeend', html)
   // make rows change colour
 
@@ -170,7 +215,7 @@ function processMeasurablesO() {
   var cat
   for (cat in candidateExample) {
     var catID = 'cat' + i 
-    if (document.getElementById(catID).checked) {
+    if (getEl(catID).checked) {
       measurables.push(cat)
     }
     i++
@@ -209,6 +254,11 @@ function processKey() {
 }
 
 
+/* HELPER FN's
+    maybe move to another file?
+*/
+
+
 // pinched from interwebs
 function deepClone(o) {
    var output, v, key
@@ -219,3 +269,7 @@ function deepClone(o) {
    }
    return output
 }
+
+function getEl(element) {
+  return document.getElementById(element)
+} 
